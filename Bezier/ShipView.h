7f377ee8433f6981cb1f767e9536be59b5a2834c
qinmin <qinmin@qinmindeMacBook-Pro.local>
2017-01-16 22:19:12 +0800

    add ship

---
+++ Bezier/ShipView.h
@@ -0,0 +1,13 @@
+//
+//  ShipView.h
+//  Bezier
+//
+//  Created by qinmin on 2017/1/16.
+//  Copyright © 2017年 qinmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShipView : UIView
+
+@end
---
+++ Bezier/ShipView.m
@@ -0,0 +1,21 @@
+//
+//  ShipView.m
+//  Bezier
+//
+//  Created by qinmin on 2017/1/16.
+//  Copyright © 2017年 qinmin. All rights reserved.
+//
+
+#import "ShipView.h"
+
+@implementation ShipView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
